00b41f746e371da9fe871efd9c9518ed7914536a
carlos.hudson <carlos.hudson@hp.com>
2019-10-10 15:54:43 -0500

    Lesson 11. All Longest Strings

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,17 @@
 function addTwoDigits(n: any): number {
-
+    const numbers = n.toString().split('');
+    
+    return numbers.reduce((acc: string, num: string) => {
+        return parseInt(acc) + parseInt(num);
+    })
 }
 
-// console.log(addTwoDigits(29));
+//Another Approach
+function addTwoDigits2(n: any): number {
+    const numbers = n.toString().split('');
+    
+    return parseInt(numbers[0]) + parseInt(numbers[1])
+}
+
+ console.log(addTwoDigits(29));
+ console.log(addTwoDigits2(29));
--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,29 @@
+// My solution
 function adjacentElementsProduct(inputArray: number[]): number {
+    // If we use 0 we are skipping the negative values. Better to do this way:
+    let result = inputArray[0] * inputArray[1];
 
+    for (let i = 1; i < inputArray.length; i++) {
+        if (inputArray[i - 1] * inputArray[i] > result) {
+            result = inputArray[i - 1] * inputArray[i]
+        }
+    }
+    return result;
 }
 
-//console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+//Professor's Approach 
+function adjacentElementsProduct2(inputArray: number[]): number {
+    let largestProduct = inputArray[0] * inputArray[1];
+
+    for (let i = 1; i < inputArray.length - 1; i++) {
+        const product = inputArray[i] * inputArray[i +1];
+
+        largestProduct = largestProduct < product ? product : largestProduct;
+    }
+
+    return largestProduct;
+}
+
+
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+console.log(adjacentElementsProduct2([3, 6, -2, -5, 7, 3]));
