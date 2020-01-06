8b8e851d9b67254c716388e0a6a5353eb1b22ad6
hudson21 <camihudsondiaz@ittepic.edu.mx>
2020-01-06 10:09:38 -0600

    Lesson 13. Alphabetic Shift

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,26 @@
-function allLongestStrings(inputArray: string[]): string[] {
+/*
+1. Create an array to push the longest strings
+2. Count the longest length
+3. Push the longest strings into the array
+*/
 
-}
+// My approach
+function allLongestStrings(inputArray: string[]): string[] {
+  let length = 0;
+  const array = [];
+  inputArray.forEach(index => {
+    length = index.length > length ? index.length : length; 
+  })
+
+  inputArray.forEach((index: string) => {
+    if (index.length === length) {
+      array.push(index);
+    }
+  });
+
+  return array;
+} // Time Complexity --> O(n)
+
+
 
 console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,50 @@
-function alphabeticShift(inputString: string): string {
+/*
+  1. Define a dictionary
+  2. Split the word in characters
+  3. Look for the character in the dictionary add + 1 to replace with the next one
+*/
 
-}
+// My Approach
+function alphabeticShift(inputString: string): string {
+  const dictionary = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  let word = inputString.split('');
+  let indexOfCharacter = -1;
+
+  for (let i = 0; i < word.length; i++) {
+    
+    if (word[i] === 'z') {
+      word[i] = 'a';
+    } else {
+      indexOfCharacter = dictionary.indexOf(word[i]);
+
+      if (indexOfCharacter !== -1) {
+        word[i] = dictionary[indexOfCharacter + 1];
+      }
+    }
+  }
+
+  return word.join('');
+} // Time Complexity --> O(n^2)
+  // Space Complexity --> O(n)
 
 console.log(alphabeticShift('crazy'));
+console.log(alphabeticShift('abcde'));
+console.log(alphabeticShift('abcdz'));
+
+
+// Professor's Approach
+function alphabeticShift1(inputString: string): string {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  let inputShifted = inputString.split('');
+
+  for (let i = 0; inputShifted.length; i++) {
+    let index = 0;
+
+    if (inputShifted[i] !== 'z') {
+      index = alphabet.indexOf(inputShifted[i] + 1); 
+    }
+
+    inputShifted
+  }
+}
+
